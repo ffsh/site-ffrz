55eaf4603e090bdc6788b6c14531ee1b6b8bb9d7
Benjamin Brahmer <info@b-brahmer.de>
2018-05-25 13:27:53 +0200

    arp filter

--- site.mk
+++ site.mk
@@ -10,6 +10,7 @@ GLUON_SITE_PACKAGES := \
 	gluon-config-mode-contact-info \
 	gluon-ebtables-filter-multicast \
 	gluon-ebtables-filter-ra-dhcp \
+	gluon-ebtables-limit-arp \
 	gluon-web-admin \
 	gluon-web-network \
 	gluon-web-autoupdater \
